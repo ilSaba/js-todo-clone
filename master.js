2dcfbbe5c77e0a02088abd5dfd90c63761d4c4d4
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-18 00:05:10 +0100

    Esercizio JS completato. Css da perfezionare.

--- master.js
+++ master.js
@@ -1,14 +1,34 @@
 // Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
 $(document).ready(function () {
-     var template = $("#todo li").clone();
-     $("#todos").append(template);
 
-     $('#todos span').click(function (){
-        $(this).parent().remove();
-     })
-
-     $("#btn-input").click(function (){
+    $("#btn-input").click(function (){
         var input = $("#input").val();
-        $("#todos").append(input);
-     })
+        $("#input").val('');
+
+        var template = $("#todo li").clone();
+
+        template.children("span.title").text(input);
+
+        template.children("span.delete").click(function () {
+            $(this).parent().remove();
+    });
+
+    template.children("span.edit").click(function (){
+        var title = $(this).siblings("span.title").text();
+        $(this).siblings("span.title").hide();
+
+        $(this).siblings("input").val(title).show();
+
+    });
+
+    template.children("input").keyup(function (e) {
+        if (e.which == 13) {
+            var title = $(this).val();
+            $(this).hide();
+            $(this).siblings("span.title").text(title).show();
+        }
+    });
+    $("#todos").append(template);
+    });
+    
 });
